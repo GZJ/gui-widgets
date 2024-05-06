eca6c01a2b3100d757d414d48f60aaf362523e06
gzj <gzj00@outlook.com>
2024-05-06 18:24:44 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gui-widgets',
+    version='0.1.0',
+    description='some gui widgets',
+    author='gzj',
+    author_email='gzj00@outlook.com',
+    url='https://github.com/gzj/gui-widgets',
+    scripts=['gk-list.py'],
+    install_requires=['PyQt5'],
+)
